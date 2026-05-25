7107e390ef38513bacfbbd9e386c58fd30177938
Sherlock Holmes <sherlockholmesxp@gmail.com>
2026-05-25 17:51:32 +0800

    fix: link colour + lazy loading fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import './layout.css';
 
 	let { children } = $props();
+
+	onMount(() => {
+		void import('./font-variants.css');
+	});
 </script>
 
-<svelte:head><link rel="icon" href="/icon.svg" /></svelte:head>
+<svelte:head>
+	<link rel="icon" href="/icon.svg" />
+	<link
+		rel="preload"
+		href="/fonts/sarasa-mono-sc-regular-nerd-font.woff2"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+</svelte:head>
 {@render children()}
